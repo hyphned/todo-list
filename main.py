e3972996de5c0b4a6cbbb8988a420b71814cccce
hyphned <gervontadavis980@gmail.com>
2025-11-09 22:14:46 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+def to_do_list():
+    tasks = []
+
+    while True:
+        print("\n To-Do List Menu:")
+        print("1. View Tasks")
+        print("2. Add task")
+        print("3. Remove task")
+        print("4. Exit")
+
+        choice = input("select an option: ").strip()
+
+        if choice == "1":
+            if tasks:
+                print("Your tasks: ")
+                for i, task in enumerate(tasks, 1):
+                    print(f"{i}. {task}")
+            else:
+                print("No tasks added yet. ")
+
+        elif choice == "2":
+            task = input("Enter a task: ").strip()
+            tasks.append(task)
+            print(f"{task} has been added to the list")
+
+        elif choice == "3":
+            task_num = int(input("Enter the task number to remove: "))
+            if 0 < task_num <= len(tasks):
+                removed_task = tasks.pop(task_num - 1)
+                print(f"Task '{removed_task}' removed.")
+            else:
+                print("Invalid task number. ")
+
+        elif choice == "4":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option")
+
+
+to_do_list()
